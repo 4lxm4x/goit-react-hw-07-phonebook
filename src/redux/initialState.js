46d7c760677a5264c2da169df9541e7b5143b9e5
Oleksandr Maksymenko <machinery69@gmail.com>
2024-04-03 12:49:49 +0300

    changed initialState

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,1 +1,8 @@
-export const initialState = { contacts: [], filter: '' };
+export const initialState = {
+  contacts: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
+  filter: '',
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,22 +4,7 @@ import { reducer } from './reducers/reducer';
 import { configureStore } from '@reduxjs/toolkit';
 // import { contactsReducer } from './reducers/contactsReducer';
 // import { filterReducer } from './reducers/filterReducer';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['filter'],
-};
-const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+  reducer,
 });
-
-export const persistor = persistStore(store);
